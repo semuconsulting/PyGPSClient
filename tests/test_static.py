63f5505fdf6fc9b95abfc45db3058a23f5ea6d55
Steve Smith <semuadmin@users.noreply.github.com>
2020-11-03 12:42:27 +0000

    static tests updated to accommodate floating point diffs

--- tests/test_static.py
+++ tests/test_static.py
@@ -7,6 +7,7 @@ Static method tests for pygpsclient.globals
 '''
 
 import unittest
+from pytest import approx
 
 from pygpsclient.globals import *
 
@@ -21,11 +22,12 @@ class StaticTest(unittest.TestCase):
 
     def testdeg2rad(self):
         res = deg2rad(147)
-        self.assertEqual(res, 2.5656340004316647)
+        assert res == approx(2.565634)
 
     def testcel2cart(self):
-        res = cel2cart(34, 128)
-        self.assertEqual(res, (-0.5104064950196395, 0.6532905223173859))
+        (elev, azim) = cel2cart(34, 128)
+        assert elev == approx(-0.510406)
+        assert azim == approx(0.653290)
 
     def testdeg2dms(self):
         res = deg2dms(53.346, 'lat')
@@ -37,31 +39,31 @@ class StaticTest(unittest.TestCase):
 
     def testm2ft(self):
         res = m2ft(39.234)
-        self.assertEqual(res, 128.72047656)
+        assert res == approx(128.720476)
 
     def testft2m(self):
         res = ft2m(124.063)
-        self.assertEqual(res, 37.81440118993916)
+        assert res == approx(37.814401)
 
     def testms2kmph(self):
         res = ms2kmph(3.654)
-        self.assertEqual(res, 13.1544)
+        assert res == approx(13.154400)
 
     def testms2mph(self):
         res = ms2mph(3.654)
-        self.assertEqual(res, 8.17376684796)
+        assert res == approx(8.173766)
 
     def testms2knots(self):
         res = ms2knots(3.654)
-        self.assertEqual(res, 7.1028057933)
+        assert res == approx(7.102805)
 
     def testkmph2ms(self):
         res = kmph2ms(3.654)
-        self.assertEqual(res, 1.0150000812)
+        assert res == approx(1.015000)
 
     def testknots2ms(self):
         res = knots2ms(3.654)
-        self.assertEqual(res, 1.8797810521896)
+        assert res == approx(1.879781)
 
     def testpos2iso6709(self):
         res = pos2iso6709(53.12, -2.165, 35)
