22ddd9e636b8862f652baa2a89bd6056ff918ca8
Steve Smith <semuadmin@users.noreply.github.com>
2020-12-04 10:13:22 +0000

    minor updates to test cases and color tags

--- tests/test_static.py
+++ tests/test_static.py
@@ -7,7 +7,6 @@ Static method tests for pygpsclient.globals
 '''
 
 import unittest
-from pytest import approx
 
 from pygpsclient.globals import *
 
@@ -22,12 +21,12 @@ class StaticTest(unittest.TestCase):
 
     def testdeg2rad(self):
         res = deg2rad(147)
-        assert res == approx(2.565634)
+        self.assertAlmostEqual(res, 2.565634, 5)
 
     def testcel2cart(self):
         (elev, azim) = cel2cart(34, 128)
-        assert elev == approx(-0.510406)
-        assert azim == approx(0.653290)
+        self.assertAlmostEqual(elev, -0.510406, 5)
+        self.assertAlmostEqual(azim, 0.653290, 5)
 
     def testdeg2dms(self):
         res = deg2dms(53.346, 'lat')
@@ -39,31 +38,31 @@ class StaticTest(unittest.TestCase):
 
     def testm2ft(self):
         res = m2ft(39.234)
-        assert res == approx(128.720476)
+        self.assertAlmostEqual(res, 128.720476, 5)
 
     def testft2m(self):
         res = ft2m(124.063)
-        assert res == approx(37.814401)
+        self.assertAlmostEqual(res, 37.814401, 5)
 
     def testms2kmph(self):
         res = ms2kmph(3.654)
-        assert res == approx(13.154400)
+        self.assertAlmostEqual(res, 13.154400, 5)
 
     def testms2mph(self):
         res = ms2mph(3.654)
-        assert res == approx(8.173766)
+        self.assertAlmostEqual(res, 8.173766, 5)
 
     def testms2knots(self):
         res = ms2knots(3.654)
-        assert res == approx(7.102805)
+        self.assertAlmostEqual(res, 7.102805, 5)
 
     def testkmph2ms(self):
         res = kmph2ms(3.654)
-        assert res == approx(1.015000)
+        self.assertAlmostEqual(res, 1.015000, 5)
 
     def testknots2ms(self):
         res = knots2ms(3.654)
-        assert res == approx(1.879781)
+        self.assertAlmostEqual(res, 1.879781, 5)
 
     def testpos2iso6709(self):
         res = pos2iso6709(53.12, -2.165, 35)
