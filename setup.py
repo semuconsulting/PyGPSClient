310ac4ac64b723a8a3715901593c2cb746f0d111
semuadmin <semuadmin@users.noreply.github.com>
2021-07-13 07:55:26 +0100

    update min pyubx2 version to 1.0.12

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     version=VERSION,
     packages=find_packages(exclude=["tests", "references", "images"]),
     install_requires=[
-        "pyubx2>=1.0.11",
+        "pyubx2>=1.0.12",
         "pynmeagps>=1.0.2",
         "requests>=2.24.0",
         "Pillow>=7.2.0",
