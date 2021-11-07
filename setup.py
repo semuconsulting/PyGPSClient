c547acf7a0f82159da80410225f03b9beed897f4
semuadmin <semuadmin@users.noreply.github.com>
2021-11-07 18:17:02 +0000

    Revert "remove Python 3.10 compatibility"
    This reverts commit 5bfe628a0ecdac6aa469092056700d8a6e8e3f97.

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Desktop Environment",
         "Topic :: Terminals :: Serial",
         "Topic :: Scientific/Engineering :: GIS",
