c4fc5228083852f19280066a20086b7456241a73
semuadmin <28569967+semuadmin@users.noreply.github.com>
2022-01-20 13:43:52 +0000

    update min pynmeagps version to 1.0.8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     packages=find_packages(exclude=["tests", "references", "images"]),
     install_requires=[
         "pyubx2>=1.2.4",
-        "pynmeagps>=1.0.7",
+        "pynmeagps>=1.0.8",
         "requests>=2.24.0",
         "Pillow>=7.2.0",
         "pyserial>=3.4",
