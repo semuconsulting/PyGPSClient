950be3b2ed239ce9ebfdcfcf669be1a462a6ef1d
semuadmin <28569967+semuadmin@users.noreply.github.com>
2022-05-06 19:37:01 +0100

    update min pyubx2 version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     version=VERSION,
     packages=find_packages(exclude=["tests", "references", "images"]),
     install_requires=[
-        "pyubx2>=1.2.8",
+        "pyubx2>=1.2.9",
         "pynmeagps>=1.0.10",
         "requests>=2.24.0",
         "Pillow>=7.2.0",
