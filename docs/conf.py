95a2dfe4be98098e0dd43edca7fcae67c68ccdb0
semuadmin <28569967+semuadmin@users.noreply.github.com>
2022-05-07 04:09:18 +0100

    update gitignore to include docs config

---
+++ docs/conf.py
@@ -0,0 +1,71 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+# document local package rather than installed one
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+currdir = os.path.dirname(__file__)
+from pygpsclient import version as VERSION
+
+# sys.path.insert(0, os.path.abspath('../pygpsclient'))
+
+# -- Project information -----------------------------------------------------
+
+project = "PyGPSClient"
+copyright = "2021, SEMU Consulting"
+author = "SEMU Consulting"
+
+# The full version, including alpha/beta/rc tags
+release = VERSION
+version = VERSION
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+html_title = "<project> v<version> documentation."
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ["_static"]
+html_last_updated_fmt = "%b %d %Y"
+html_theme_options = {
+    "display_version": True,
+}
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
+}
