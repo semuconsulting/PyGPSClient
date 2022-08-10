46bc6b7d3a46acebbaabbd14728e748884829aeb
semuadmin <28569967+semuadmin@users.noreply.github.com>
2022-08-10 07:55:34 +0100

    update min pygnssutils to 0.3.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         exclude=["tests", "references", "images", "docs", "examples"],
     ),
     install_requires=[
-        "pygnssutils>=0.3.0",
+        "pygnssutils>=0.3.1",
         "requests>=2.24.0",
         "Pillow>=7.2.0",
         "pyserial>=3.5",
