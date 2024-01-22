1a44663f8fbba64b15994ba21133a2a345eaaf79
semuadmin <28569967+semuadmin@users.noreply.github.com>
2024-01-22 18:36:38 +0000

    remove redundant comment

--- src/pygpsclient/map_frame.py
+++ src/pygpsclient/map_frame.py
@@ -185,9 +185,7 @@ class MapviewFrame(Frame):
         self._lastmaptype = "world"
         w, h = self.width, self.height
         self._can_mapview.delete("all")
-        self._img = ImageTk.PhotoImage(
-            Image.open(IMG_WORLD).resize((w, h))  # , Image.ANTIALIAS)
-        )
+        self._img = ImageTk.PhotoImage(Image.open(IMG_WORLD).resize((w, h)))
         self._marker = ImageTk.PhotoImage(Image.open(ICON_POS))
         self._can_mapview.create_image(0, 0, image=self._img, anchor=NW)
         x = (w / 2) + int((lon * (w / 360))) + OFFSET_X
