58907aa707a2a19a67331890f12ec12922adbace
SEMU Admin <28569967+semuadmin@users.noreply.github.com>
2025-10-25 10:10:17 +0100

    update example

--- examples/presets.py
+++ examples/presets.py
@@ -1,7 +1,7 @@
 """
 Illustration of how to create UBX configuration database
 messages and convert them to a UBX preset entry which can
-be inserted into the presets_l section of a .json config
+be inserted into the ubxpresets_l section of a .json config
 file.
 
 Created on 21 Feb 2024
@@ -19,5 +19,5 @@ cfgdata = [("CFG_MSGOUT_UBX_RXM_RAWX_UART1", 1), ("CFG_MSGOUT_UBX_RXM_SFRBX_UART
 
 msg = UBXMessage.config_set(layers, transaction, cfgdata)
 print(msg)
-preset = ubx2preset(msg, "Enable RAWX and SFRBR messages on UART1")
+preset = ubx2preset(msg, "Enable RAWX and SFRBX messages on UART1")
 print(preset)
