8d480860132393c6566195f60d675fcfd0707570
SEMU Admin <28569967+semuadmin@users.noreply.github.com>
2025-12-16 09:29:14 +0000

    update macos install script to Python 3.14.2

--- examples/pygpsclient_macos_install.sh
+++ examples/pygpsclient_macos_install.sh
@@ -17,7 +17,7 @@
 set -e
 
 echo "PyGPSClient will be installed at $HOME/pygpsclient/bin"
-VER=3.13.7
+VER=3.14.2
 echo "Installing Python $VER from python.org ..."
 curl https://www.python.org/ftp/python/$VER/python-$VER-macos11.pkg --output python-$VER-macos11.pkg
 sudo installer -pkg python-$VER-macos11.pkg -target /
